21f457032b3b214123d2c99e2c530ef3265e165a
Jeremiah Bintang Santoso <jeremiahbintang@gmail.com>
2021-06-08 23:44:06 +0700

    Replace quarkus with spring boot

---
+++ lokasiku-backend/lokasiku-backend/src/main/java/org/lokasiku/lokasikubackend/LokasikuBackendApplication.java
@@ -0,0 +1,13 @@
+package org.lokasiku.lokasikubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LokasikuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LokasikuBackendApplication.class, args);
+	}
+
+}
---
+++ lokasiku-backend/lokasiku-backend/src/test/java/org/lokasiku/lokasikubackend/LokasikuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lokasiku.lokasikubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LokasikuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
