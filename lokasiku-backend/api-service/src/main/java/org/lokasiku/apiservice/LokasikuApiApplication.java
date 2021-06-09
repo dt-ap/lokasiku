7701a8fc72c79d6db5a5498f43b278a6d6210928
dt-ap <dt_ap@ymail.com>
2021-06-09 09:10:25 +0700

    change

---
+++ lokasiku-backend/api-service/src/main/java/org/lokasiku/apiservice/LokasikuApiApplication.java
@@ -0,0 +1,13 @@
+package org.lokasiku.apiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LokasikuApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LokasikuApiApplication.class, args);
+  }
+
+}
--- lokasiku-backend/api-service/src/test/java/org/lokasiku/apiservice/LokasikuBackendApplicationTests.java
+++ lokasiku-backend/api-service/src/test/java/org/lokasiku/apiservice/LokasikuBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package org.lokasiku.lokasikubackend;
+package org.lokasiku.apiservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LokasikuBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
