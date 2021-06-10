8fbb7d608821dda07b249ffb8e0d02bed7a185e8
dt-ap <dt_ap@ymail.com>
2021-06-10 23:21:27 +0700

    code style

--- lokasiku-backend/api-service/src/main/java/org/lokasiku/apiservice/config/CustomPhysicalNamingStrategy.java
+++ lokasiku-backend/api-service/src/main/java/org/lokasiku/apiservice/config/CustomPhysicalNamingStrategy.java
@@ -4,7 +4,6 @@ import org.hibernate.boot.model.naming.Identifier;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
 import org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy;
 
-
 public class CustomPhysicalNamingStrategy extends SpringPhysicalNamingStrategy {
 
   @Override
