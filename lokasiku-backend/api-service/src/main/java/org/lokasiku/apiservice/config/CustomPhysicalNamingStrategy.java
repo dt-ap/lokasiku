772a968e2c12494fc9252ffe1b53550c469d109b
dt-ap <dt_ap@ymail.com>
2021-06-10 23:18:30 +0700

    model_migration

---
+++ lokasiku-backend/api-service/src/main/java/org/lokasiku/apiservice/config/CustomPhysicalNamingStrategy.java
@@ -0,0 +1,16 @@
+package org.lokasiku.apiservice.config;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+import org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy;
+
+
+public class CustomPhysicalNamingStrategy extends SpringPhysicalNamingStrategy {
+
+  @Override
+  public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+    var newName = super.toPhysicalTableName(name, jdbcEnvironment).getText();
+    return Identifier.toIdentifier(newName + "s");
+  }
+
+}
