14463f8fc3031d4e855ea3aefc4714e10f4039a2
dt-ap <dt_ap@ymail.com>
2021-06-11 16:51:12 +0700

    rename

--- lokasiku-backend/api-service/src/main/java/org/lokasiku/apiservice/config/PhysicalNamingStrategy.java
+++ lokasiku-backend/api-service/src/main/java/org/lokasiku/apiservice/config/PhysicalNamingStrategy.java
@@ -4,7 +4,7 @@ import org.hibernate.boot.model.naming.Identifier;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
 import org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy;
 
-public class CustomPhysicalNamingStrategy extends SpringPhysicalNamingStrategy {
+public class PhysicalNamingStrategy extends SpringPhysicalNamingStrategy {
 
   @Override
   public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment jdbcEnvironment) {
